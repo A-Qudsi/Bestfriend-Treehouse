d1ba419d3a1904f742096b1e5f057c88809e38a7
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2020-09-22 18:34:21 -0400

    fixed errors (maybe)

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,9 +1,9 @@
-// import { combineReducers } from "redux";
+import { combineReducers } from "redux";
 
-// import sessionErrorsReducer from "./session_errors_reducer";
+import sessionErrorsReducer from "./session_errors_reducer";
 
-// const errorsReducer = combineReducers({
-//     session: sessionErrorsReducer
-// });
+const errorsReducer = combineReducers({
+    session: sessionErrorsReducer
+});
 
-// export default errorsReducer;
+export default errorsReducer;
