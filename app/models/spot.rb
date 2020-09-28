7ae5ceb91538615221fd196d0231889ffd869e2f
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2020-09-28 12:06:46 -0400

    fixed spot Model

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,6 @@
 class Spot < ApplicationRecord
 
-    validates :name, :description, :latitude, :longitude, :maxGuests, :price, :address, :city, :host_id presence: true
+    validates :name, :description, :latitude, :longitude, :maxGuests, :price, :address, :city, :host_id, presence: true
 
     belongs_to :user,
         foreign_key: :host_id,
