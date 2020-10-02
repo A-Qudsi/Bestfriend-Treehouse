c479e2a8adc1850cedd4182e125856d4fd9ff1d0
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2020-10-02 04:02:47 -0400

    fixed user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
     validates :email, :session_token, uniqueness: true
     validates :password, length: { minimum: 6 }, allow_nil: true
 
-    has_many :spots
+    has_many :spots,
+        foreign_key: :host_id,
+        class_name: :Spot
     
     attr_reader :password
     after_initialize :ensure_session_token
