3af92b4d0faaec6f0eeaf62e8cdc73e9f4615303
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-04-07 16:35:26 -0400

    reservation/review model associations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,12 @@
 class Reservation < ApplicationRecord
+    validates :start_date, :end_date, :number_guests, presence: true
+
+    belongs_to :user,
+        foreign_key: :guest_id,
+        class_name: :User
     
+    belongs_to :spot,
+        foreign_key: :spot_id,
+        class_name: :Spot
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,12 @@
 class Review < ApplicationRecord
+    validates :body, :rating, presence: true
+
+    belongs_to :user,
+        foreign_key: :user_id,
+        class_name: :User
+
+    belongs_to :spot,
+        foreign_key: :spot_id,
+        class_name: :Spot
+        
 end
