19a0292ec21f90081c4a7bf80b05baa7dc650df9
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-04-14 00:05:10 -0400

    Made dropdown its own component

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -856,10 +856,10 @@ var Navbar = /*#__PURE__*/function (_React$Component) {
 
 /***/ }),
 
-/***/ "./frontend/components/reservations/reservation_form.js":
-/*!**************************************************************!*\
-  !*** ./frontend/components/reservations/reservation_form.js ***!
-  \**************************************************************/
+/***/ "./frontend/components/reservations/reservation_dropdown.js":
+/*!******************************************************************!*\
+  !*** ./frontend/components/reservations/reservation_dropdown.js ***!
+  \******************************************************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -867,16 +867,7 @@ var Navbar = /*#__PURE__*/function (_React$Component) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_dates__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dates */ "./node_modules/react-dates/index.js");
-/* harmony import */ var react_dates__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dates__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var react_dates_initialize__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-dates/initialize */ "./node_modules/react-dates/initialize.js");
-/* harmony import */ var react_dates_initialize__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_dates_initialize__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var moment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! moment */ "./node_modules/moment/moment.js");
-/* harmony import */ var moment__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(moment__WEBPACK_IMPORTED_MODULE_3__);
-/* harmony import */ var _reservations_index__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./reservations_index */ "./frontend/components/reservations/reservations_index.jsx");
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -900,55 +891,35 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 
 
 
+var Dropdown = /*#__PURE__*/function (_React$Component) {
+  _inherits(Dropdown, _React$Component);
 
+  var _super = _createSuper(Dropdown);
 
-
-
-var ReservationForm = /*#__PURE__*/function (_React$Component) {
-  _inherits(ReservationForm, _React$Component);
-
-  var _super = _createSuper(ReservationForm);
-
-  function ReservationForm(props) {
+  function Dropdown(props) {
     var _this;
 
-    _classCallCheck(this, ReservationForm);
+    _classCallCheck(this, Dropdown);
 
     _this = _super.call(this, props);
     _this.state = {
       counter: 1,
       childrenCounter: 0,
       infantCounter: 0,
-      clicked: false,
-      startDate: null,
-      endDaate: null,
-      show: false
+      clicked: false
     };
-    _this.handleClick = _this.handleClick.bind(_assertThisInitialized(_this));
-    _this.closeDropdown = _this.closeDropdown.bind(_assertThisInitialized(_this));
     _this.addInfantCounter = _this.addInfantCounter.bind(_assertThisInitialized(_this));
     _this.removeInfantCounter = _this.removeInfantCounter.bind(_assertThisInitialized(_this));
     _this.addChildrenCounter = _this.addChildrenCounter.bind(_assertThisInitialized(_this));
     _this.removeChildrenCounter = _this.removeChildrenCounter.bind(_assertThisInitialized(_this));
     _this.addCounter = _this.addCounter.bind(_assertThisInitialized(_this));
     _this.removeCounter = _this.removeCounter.bind(_assertThisInitialized(_this));
-    _this.bookReservation = _this.bookReservation.bind(_assertThisInitialized(_this));
+    _this.handleClick = _this.handleClick.bind(_assertThisInitialized(_this));
+    _this.closeDropdown = _this.closeDropdown.bind(_assertThisInitialized(_this));
     return _this;
   }
 
-  _createClass(ReservationForm, [{
-    key: "bookReservation",
-    value: function bookReservation(e) {
-      e.preventDefault();
-      debugger;
-
-      if (this.props.currentUser) {
-        this.props.history.push("users/".concat(this.props.currentUser.id, "/reservations}"));
-      } else {
-        this.props.openModal('login');
-      }
-    }
-  }, {
+  _createClass(Dropdown, [{
     key: "closeDropdown",
     value: function closeDropdown() {
       this.setState({
@@ -981,7 +952,7 @@ var ReservationForm = /*#__PURE__*/function (_React$Component) {
     }
   }, {
     key: "addCounter",
-    value: function addCounter() {
+    value: function addCounter(e) {
       e.preventDefault();
       this.setState({
         counter: this.state.counter + 1
@@ -1014,49 +985,9 @@ var ReservationForm = /*#__PURE__*/function (_React$Component) {
   }, {
     key: "render",
     value: function render() {
-      var _this2 = this;
-
-      var that = this;
       var spot = this.props.spot;
       var totalCounter = this.state.counter + this.state.childrenCounter;
       return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "bookingcontainer"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "bookingHeader"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "l_e"
-      }, "$ ", spot.price, " / night"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "bookingRating"
-      }, "5.0")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
-        className: "signup_form body"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "sReservation"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_dates__WEBPACK_IMPORTED_MODULE_1__["DateRangePicker"], _defineProperty({
-        block: true,
-        startDate: this.state.startDate,
-        startDateId: "mm/dd/yyyy",
-        endDate: this.state.endDate,
-        endDateId: "mm/dd/yyyy",
-        noBorder: false,
-        onDatesChange: function onDatesChange(_ref) {
-          var startDate = _ref.startDate,
-              endDate = _ref.endDate;
-          return _this2.setState({
-            startDate: startDate,
-            endDate: endDate
-          });
-        },
-        focusedInput: this.state.focusedInput,
-        onFocusChange: function onFocusChange(focusedInput) {
-          return _this2.setState({
-            focusedInput: focusedInput
-          });
-        },
-        numberOfMonths: 1,
-        hideKeyboardShortcutsPanel: true,
-        startDatePlaceholderText: "Check-in",
-        endDatePlaceholderText: "Checkout"
-      }, "block", true))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "sGuests"
       }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
         className: "sGuestsButton",
@@ -1124,7 +1055,149 @@ var ReservationForm = /*#__PURE__*/function (_React$Component) {
         className: "disclousreGuests"
       }, "the number of guests."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         onClick: this.closeDropdown
-      }, " close")) : null))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+      }, " close")) : null)));
+    }
+  }]);
+
+  return Dropdown;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+
+;
+/* harmony default export */ __webpack_exports__["default"] = (Dropdown);
+
+/***/ }),
+
+/***/ "./frontend/components/reservations/reservation_form.js":
+/*!**************************************************************!*\
+  !*** ./frontend/components/reservations/reservation_form.js ***!
+  \**************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dates__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dates */ "./node_modules/react-dates/index.js");
+/* harmony import */ var react_dates__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dates__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react_dates_initialize__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-dates/initialize */ "./node_modules/react-dates/initialize.js");
+/* harmony import */ var react_dates_initialize__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_dates_initialize__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var moment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! moment */ "./node_modules/moment/moment.js");
+/* harmony import */ var moment__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(moment__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _reservations_index__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./reservations_index */ "./frontend/components/reservations/reservations_index.jsx");
+/* harmony import */ var _reservation_dropdown__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./reservation_dropdown */ "./frontend/components/reservations/reservation_dropdown.js");
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+
+
+
+
+
+
+
+var ReservationForm = /*#__PURE__*/function (_React$Component) {
+  _inherits(ReservationForm, _React$Component);
+
+  var _super = _createSuper(ReservationForm);
+
+  function ReservationForm(props) {
+    var _this;
+
+    _classCallCheck(this, ReservationForm);
+
+    _this = _super.call(this, props);
+    _this.state = {
+      clicked: false,
+      startDate: null,
+      endDaate: null,
+      show: false
+    };
+    _this.bookReservation = _this.bookReservation.bind(_assertThisInitialized(_this));
+    return _this;
+  }
+
+  _createClass(ReservationForm, [{
+    key: "bookReservation",
+    value: function bookReservation(e) {
+      e.preventDefault();
+      debugger;
+
+      if (this.props.currentUser) {
+        this.props.history.push("users/".concat(this.props.currentUser.id, "/reservations}"));
+      } else {
+        this.props.openModal('login');
+      }
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      var that = this;
+      var spot = this.props.spot;
+      var totalCounter = this.state.counter + this.state.childrenCounter;
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "bookingcontainer"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "bookingHeader"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "l_e"
+      }, "$ ", spot.price, " / night"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "bookingRating"
+      }, "5.0")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
+        className: "signup_form body"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "sReservation"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_dates__WEBPACK_IMPORTED_MODULE_1__["DateRangePicker"], _defineProperty({
+        block: true,
+        startDate: this.state.startDate,
+        startDateId: "mm/dd/yyyy",
+        endDate: this.state.endDate,
+        endDateId: "mm/dd/yyyy",
+        noBorder: false,
+        onDatesChange: function onDatesChange(_ref) {
+          var startDate = _ref.startDate,
+              endDate = _ref.endDate;
+          return _this2.setState({
+            startDate: startDate,
+            endDate: endDate
+          });
+        },
+        focusedInput: this.state.focusedInput,
+        onFocusChange: function onFocusChange(focusedInput) {
+          return _this2.setState({
+            focusedInput: focusedInput
+          });
+        },
+        numberOfMonths: 1,
+        hideKeyboardShortcutsPanel: true,
+        startDatePlaceholderText: "Check-in",
+        endDatePlaceholderText: "Checkout"
+      }, "block", true))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_reservation_dropdown__WEBPACK_IMPORTED_MODULE_5__["default"], {
+        spot: spot
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
         className: "submit-button",
         onClick: function onClick(e) {
           return _this2.bookReservation(e);
--- frontend/components/reservations/reservation_form.js
+++ frontend/components/reservations/reservation_form.js
@@ -3,28 +3,19 @@ import { DateRangePicker } from 'react-dates';
 import 'react-dates/initialize';
 import moment from 'moment';
 import ReservationsIndex from './reservations_index';
+import Dropdown from './reservation_dropdown';
 
 class ReservationForm extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            counter: 1,
-            childrenCounter: 0,
-            infantCounter: 0,
             clicked: false,
             startDate: null,
             endDaate: null,
             show: false,
         };
-        this.handleClick = this.handleClick.bind(this);
-        this.closeDropdown = this.closeDropdown.bind(this);
-        this.addInfantCounter = this.addInfantCounter.bind(this);
-        this.removeInfantCounter= this.removeInfantCounter.bind(this);
-        this.addChildrenCounter = this.addChildrenCounter.bind(this);
-        this.removeChildrenCounter = this.removeChildrenCounter.bind(this);
-        this.addCounter = this.addCounter.bind(this);
-        this.removeCounter = this.removeCounter.bind(this);
+
         this.bookReservation = this.bookReservation.bind(this);
     }
 
@@ -37,40 +28,7 @@ class ReservationForm extends React.Component {
             this.props.openModal('login');
         }
     }
-    closeDropdown() {
-        this.setState({clicked: false})
-    }
 
-    handleClick(e) {
-        e.preventDefault();
-        this.setState({ clicked: true })
-    }
-
-    addInfantCounter(e) {
-        e.preventDefault();
-        this.setState({ infantCounter: this.state.infantCounter + 1 })
-    }   
-    addChildrenCounter(e) {
-        e.preventDefault();
-        this.setState({ childrenCounter: this.state.childrenCounter + 1 })
-    }   
-    addCounter() {
-        e.preventDefault();
-        this.setState({ counter: this.state.counter + 1 })
-    }   
-
-    removeInfantCounter(e) {
-        e.preventDefault();
-        this.setState({ infantCounter: this.state.infantCounter - 1 })
-    }
-    removeChildrenCounter(e) {
-        e.preventDefault();
-        this.setState({ childrenCounter: this.state.childrenCounter - 1 })
-    }
-    removeCounter(e) {  
-        e.preventDefault();
-        this.setState({ counter: this.state.counter - 1 })
-    }
 
     render() {
         const that = this;
@@ -107,58 +65,7 @@ class ReservationForm extends React.Component {
                                     block={true}
                                 />
                             </div>
-                            <div className='sGuests' >
-                                <button className='sGuestsButton'  onClick={this.handleClick} > 
-                                    <div className='sGuestButtonDiv' >
-                                        <div>{totalCounter} guest{this.state.infantCounter === 0 ? null : this.state.infantCounter === 1 ? ", " + this.state.infantCounter + " infant" : ", " + this.state.infantCounter + " infants"  }</div>
-                                        {!this.state.clicked ? (
-                                        <i className="fas fa-chevron-down"></i> ) : <i className="fas fa-chevron-up"></i> }
-                                        {this.state.clicked ? (
-                                            <ul onClick={e => e.stopPropagation()} id='reservation-dropdown'>   
-                                                <div className='Adults'>
-                                                <span>Adults</span>
-                                                <div className='dropdownAdds'>
-                                                    {totalCounter <= 1  || this.state.counter === 1 ? <i className="far fa-minus-square gray" ></i> :
-                                                    <i className="far fa-minus-square" onClick={this.removeCounter}></i> }
-                                                    {this.state.counter}
-                                                    {totalCounter >= spot.maxGuests ? <i className="far fa-plus-square gray" ></i> :
-                                                    <i className="far fa-plus-square" onClick={this.addCounter}></i> }
-                                                </div>
-                                                </div>
-                                                <div className='Adults'>
-                                                <div  className='dropdownText'>
-                                                    <span>Children</span>
-                                                    <span>Ages 2–12</span>
-                                                </div>
-                                                <div className='dropdownAdds'>
-                                                    {totalCounter <= 1  || this.state.childrenCounter === 0 ? <i className="far fa-minus-square gray" ></i> :
-                                                    <i className="far fa-minus-square" onClick={this.removeChildrenCounter}></i> }
-                                                    {this.state.childrenCounter}
-                                                    {totalCounter >= spot.maxGuests ? <i className="far fa-plus-square gray" ></i> :
-                                                    <i className="far fa-plus-square" onClick={this.addChildrenCounter}></i> }
-                                                </div>
-                                                </div>
-                                                <div className='Adults'>
-                                                <div className='dropdownText'> 
-                                                    <span>Infants</span>
-                                                    <span>Under 2</span>
-                                                </div>
-                                                <div className='dropdownAdds'>
-                                                    { this.state.infantCounter <= 0 ? <i className="far fa-minus-square gray" ></i> : 
-                                                    <i className="far fa-minus-square" onClick={this.removeInfantCounter}></i> }
-                                                    {this.state.infantCounter}
-                                                    {this.state.infantCounter >= 5 ? <i className="far fa-plus-square gray" ></i> : 
-                                                    <i className="far fa-plus-square" onClick={this.addInfantCounter}></i> }
-                                                </div>
-                                                </div>
-                                                <span className = 'disclousreGuests'>{spot.maxGuests} guests maximum. Infants don’t count toward </span>
-                                                <span className = 'disclousreGuests'>the number of guests.</span>
-                                                <div onClick={this.closeDropdown}> close</div>
-                                            </ul>
-                                        ) : null}
-                                    </div>  
-                                </button>
-                            </div>
+                            <Dropdown spot={spot} />
                             <button className='submit-button' onClick={(e) => this.bookReservation(e)
                             }>Reserve</button>
                             {this.state.show ? <ReservationsIndexContainer
@@ -177,5 +84,3 @@ class ReservationForm extends React.Component {
 };
 
 export default ReservationForm;
-
-                        
