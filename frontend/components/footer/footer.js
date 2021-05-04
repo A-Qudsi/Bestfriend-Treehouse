98560f23cecbb37bfe76cf7e2a4380c8758b50f8
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-04 04:48:56 -0400

    updated footer

--- frontend/components/footer/footer.js
+++ frontend/components/footer/footer.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='footer'>
-            <p>© 2020 Bestfriend TreeHouse · Adel Qudsi · Github · Linkedin · Portfolio</p>
-        </div>
-    )
+      <div className="footer">
+        <span>© 2020 Bestfriend TreeHouse · Adel Qudsi · Portfolio · </span>
+        <span>
+          <a href="https://www.linkedin.com/in/adel-qudsi/">Linkedin · </a>
+        </span>
+        <span>
+          <a href="https://github.com/A-Qudsi/Bestfriend-Treehouse">Github</a>
+        </span>
+      </div>
+    );
 }
 
 export default Footer
