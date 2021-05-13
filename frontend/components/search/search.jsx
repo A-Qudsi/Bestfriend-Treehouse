74f536ba96a3486504c35cabce31abf46e7675a4
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-13 07:33:28 -0400

    commented out search

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
+// import React from 'react';
 
-// import FilterForm from './filter_form';
-import SpotsPage from '../spots/spots_index';
-import SpotsMap from '../spots/spots_map';
+// // import FilterForm from './filter_form';
+// import SpotsPage from '../spots/spots_index';
+// import SpotsMap from '../spots/spots_map';
  
-const Search = ({ spots }) => (
-  <div className="user-panel">
-    <div className="left-half">
-      <h5>Click Map to Add Bench!</h5>
-      <SpotsMap
-        spots={spots}
-      />
-    </div>
-    <div className="right-half">
-    {/* //   <FilterForm */}
-    {/* //     minSeating={minSeating}
-    //     maxSeating={maxSeating}
-    //     updateFilter={updateFilter}
-    //   /> */}
-        <SpotsPage spots={spots} />
-    </div>
-  </div>
-);
+// const Search = ({ spots }) => (
+//   <div className="user-panel">
+//     <div className="left-half">
+//       <SpotsMap
+//         spots={spots}
+//       />
+//     </div>
+//     <div className="right-half">
+//     {/* //   <FilterForm */}
+//     {/* //     minSeating={minSeating}
+//     //     maxSeating={maxSeating}
+//     //     updateFilter={updateFilter}
+//     //   /> */}
+//         <SpotsPage spots={spots} />
+//     </div>
+//   </div>
+// );
 
-export default Search;
+// export default Search;
