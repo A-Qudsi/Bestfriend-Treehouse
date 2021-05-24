b9d149a43cf6edcf475a44ee1a617d3fcf2ca9b8
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-23 23:25:39 -0400

    updated Splash

--- frontend/components/splash/splash.js
+++ frontend/components/splash/splash.js
@@ -4,17 +4,15 @@ import BackgroundBody from "./background_body";
 import Cards from "./cards";
 import Disclaimer from "./disclaimer";
 
-class Splash extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <BackgroundBody />
-        <Cards />
-        <Escapes />
-        <Disclaimer />
-      </Fragment>
-    );
-  }
-}
+const Splash = () => {
+  return (
+    <Fragment>
+      <BackgroundBody />
+      <Cards />
+      <Escapes />
+      <Disclaimer />
+    </Fragment>
+  );
+};
 
 export default Splash;
