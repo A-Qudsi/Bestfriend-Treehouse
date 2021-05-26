931c61ae72c04aed15672536bc25b8c38eb3d924
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-25 23:30:19 -0400

    updated reviews and spots controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,5 +1,5 @@
 class Api::ReviewsController < ApplicationController 
-    before_action :ensure_logged_in
+    before_action :ensure_logged_in, only: [:create, :destroy, :update]
 
     def index 
         @reviews = Review.all
--- app/controllers/api/spots_controller.rb
+++ app/controllers/api/spots_controller.rb
@@ -1,8 +1,7 @@
 class Api::SpotsController < ApplicationController
 
     def index 
-        debugger
-        @spots = Spot.in_bounds(params[:bounds])
+        @spots = Spot.all
         render :index
     end
 
