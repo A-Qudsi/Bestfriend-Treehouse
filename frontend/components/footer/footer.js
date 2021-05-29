28522b8892300b67231fb56e2d7ae47a4d760f80
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-29 19:20:39 -0400

    updated footer

--- frontend/components/footer/footer.js
+++ frontend/components/footer/footer.js
@@ -3,7 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <span>© 2020 Bestfriend TreeHouse · Adel Qudsi · Portfolio · </span>
+      <span>© 2020 Bestfriend TreeHouse · Adel Qudsi · </span>
+      <span>
+        <a href="http://adelqudsi.com/">Portfolio · </a>
+      </span>
       <span>
         <a href="https://www.linkedin.com/in/adel-qudsi/">Linkedin · </a>
       </span>
