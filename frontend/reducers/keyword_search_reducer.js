15bef98b236bef2a97e97ec8c76cc9085b8bcc65
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-05-31 02:09:17 -0400

    keyword

--- frontend/reducers/keyword_search_reducer.js
+++ frontend/reducers/keyword_search_reducer.js
@@ -1,14 +1,14 @@
-import { FETCH_KEYWORD, DELETE_KEYWORD } from "../actions/keyword_actions";
+// import { FETCH_KEYWORD, DELETE_KEYWORD } from "../actions/keyword_actions";
 
-const keywordSearchReducer = (state = null, action) => {
-  switch (action.type) {
-    case FETCH_KEYWORD:
-      return action.keyword;
-    case DELETE_KEYWORD:
-      return null;
-    default:
-      return state;
-  }
-}
+// const keywordSearchReducer = (state = null, action) => {
+//   switch (action.type) {
+//     case FETCH_KEYWORD:
+//       return action.keyword;
+//     case DELETE_KEYWORD:
+//       return null;
+//     default:
+//       return state;
+//   }
+// }
 
-export default keywordSearchReducer
+// export default keywordSearchReducer
