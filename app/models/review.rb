51feff70fc91b697e460983d5389350ea0e36938
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2021-07-20 17:35:22 -0400

    update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
     validates :body, :rating, presence: true
     validates :rating, inclusion: { in: 1..5 }
+    # validates :spot_id, uniqueness: { scope: :user_id, message: "You've already reviewed this site, please edit previous review" }
+
+    before_create :not_reviewed?
 
     belongs_to :user,
         foreign_key: :user_id,
@@ -9,5 +12,10 @@ class Review < ApplicationRecord
     belongs_to :spot,
         foreign_key: :spot_id,
         class_name: :Spot
+
+    def not_reviewed? 
+        return if Review.exists?(user: user, spot_id: spot_id)
+    end
+
         
 end
